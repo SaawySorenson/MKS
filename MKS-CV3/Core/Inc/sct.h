86fa675fbe088721d8265b651bc6628977d5becb
Tomáš Svěcený <tomasek191000@gmail.com>
2023-10-11 15:46:48 +0200

    Signed-off-by: Tomáš Svěcený <tomasek191000@gmail.com>  sct_led running

---
+++ MKS-CV3/Core/Inc/sct.h
@@ -0,0 +1,14 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 11, 2023
+ *      Author: xsvece00
+ */
+
+#ifndef SCT_H_
+#define SCT_H_
+
+void sct_init();
+void sct_led(uint32_t value);
+
+#endif /* SCT_H_ */
---
+++ MKS-CV3/Core/Src/sct.c
@@ -0,0 +1,41 @@
+/*
+ * sct.c
+ *
+ *  Created on: Oct 11, 2023
+ *      Author: xsvece00
+ */
+#include "main.h"
+#include "sct.h"
+
+void sct_init()
+{
+	sct_led(0);
+}
+
+static void tick()
+{
+	HAL_GPIO_WritePin(SCT_CLK_GPIO_Port, SCT_CLK_Pin, 0);
+	HAL_Delay(5);
+	HAL_GPIO_WritePin(SCT_CLK_GPIO_Port, SCT_CLK_Pin, 1);
+}
+
+// value = 0b0001101010.... 32b
+void sct_led(uint32_t value)
+{
+	uint32_t pos = 0X00000001;
+
+	for(uint32_t i = 0; i < 32; i++)
+	{
+		if((value >> i) & (pos == 1))
+		{
+			HAL_GPIO_WritePin(SCT_SDI_GPIO_Port, SCT_SDI_Pin, 1);
+			tick();
+		}
+		else
+		{
+			HAL_GPIO_WritePin(SCT_SDI_GPIO_Port, SCT_SDI_Pin, 0);
+			tick();
+		}
+	}
+	HAL_GPIO_WritePin(SCT_NLA_GPIO_Port, SCT_NLA_Pin, 1);
+}
